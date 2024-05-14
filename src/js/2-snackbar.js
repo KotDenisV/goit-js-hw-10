25052a745e229f77bacce884aaefe430fbec8bd2
KotDenisV <kot.denis@gmail.com>
2024-05-14 10:29:03 +0300

    promis js final 14/05 10:28

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,48 +3,42 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector(".form");
 const input = document.querySelector("input[type='number']");
-const checkbox = document.querySelector("input[type='radio']");
-let state;
-let delay;
+const button = document.querySelector("button[type='submit']");
+button.classList.add('btn-submit');
 
-console.log(form);
-
-input.addEventListener("input", handleInput);
-form.addEventListener("change", handleChange);
 form.addEventListener("submit", handleSubmit);
 
-function handleInput(event) {
-    delay = event.target.value;
-    console.log(delay);
-}
-
-function handleChange(event) {
-    state = event.target.value;
-    console.log(state);
-}
-
-
 function handleSubmit(event) {
+    const delay = Number(input.value);
+    const state = document.querySelector("[name='state']:checked").value;
     event.preventDefault();
+    
     const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (state === "fulfilled") {
-        resolve(iziToast.show({
+        resolve(delay);
+      } else {
+        reject(delay);
+      }    
+    }, delay);
+    });
+    
+    promise
+    .then(delay => {
+      iziToast.show({
           title: `✅ Fulfilled promise in ${delay}ms`,
           titleColor: 'white',  
           backgroundColor: 'green',
           position: 'topRight'
-        }));
-      } else {
-        reject(iziToast.show({
+        });
+    })
+    .catch(delay => {
+      iziToast.show({
           title: `❌ Rejected promise in ${delay}ms`,
           titleColor: 'white',
           backgroundColor: 'red',
           position: 'topRight'
-        }));
-      }
-    }, delay);
+        });
     });
-    checkbox.checked = false;
-    input.value = '';
+    form.reset();
 }
